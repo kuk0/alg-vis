dd1200946d24329676cfb328168f126f41eac49d
Viktor Tomkovič <viktor.tomkovic@gmail.com>
2011-10-25 22:56:25 +0200

    TR divided to four runs to simplify the idea behind it and (hopefully) better debugging. :) (and understanding and easier shift to m-ary trees etc.) One big change against TR - offset mean offset from parent not offsets of sons.
    ExtremePair is basically a pointer to left&right extreme - i think its
    better to store it this way, because I am often losing tracks on all of
    these references in function arguments and returning classes etc.
    
    Signed-off-by: Viktor Tomkovič <viktor.tomkovic@gmail.com>

--- src/algvis/bst/BSTNode.java
+++ src/algvis/bst/BSTNode.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import algvis.core.DataStructure;
+import algvis.core.ExtremePair;
 import algvis.core.Node;
 import algvis.core.View;
 
 public class BSTNode extends Node {
 	public int leftw, rightw;
 	public BSTNode left = null, right = null, parent = null;
+
+	// variables for TR, probably there will be some changes to offset variables
+	public int xofffromparent; // offset from parent node
+	public int level; // "height" from root
+	public boolean thread; // is this node threaded?
 
 	// statistics
 	public int size = 1, height = 1, sumh = 1;
@@ -53,8 +59,8 @@ public class BSTNode extends Node {
 	}
 
 	/**
-	 * Calculate the height, size, and sum of heights of this node,
-	 * assuming that this was already calculated for its children.
+	 * Calculate the height, size, and sum of heights of this node, assuming
+	 * that this was already calculated for its children.
 	 */
 	public void calc() {
 		int ls = 0, rs = 0, lh = 0, rh = 0, lsh = 0, rsh = 0;
@@ -74,8 +80,8 @@ public class BSTNode extends Node {
 	}
 
 	/**
-	 * Calculate the height, size, and sum of heights for all the nodes in this subtree
-	 * (recursively bottom-up).
+	 * Calculate the height, size, and sum of heights for all the nodes in this
+	 * subtree (recursively bottom-up).
 	 */
 	public void calcTree() {
 		if (left != null) {
@@ -117,13 +123,15 @@ public class BSTNode extends Node {
 
 	/**
 	 * Create an (imaginary) box around the subtree rooted at this node.
-	 * Calculate the width from the node to the left side (leftw)
-	 * and the width from the node to the right side (rightw).
-	 * Assumption: this box has already been created for both children. 
+	 * Calculate the width from the node to the left side (leftw) and the width
+	 * from the node to the right side (rightw). Assumption: this box has
+	 * already been created for both children.
 	 */
 	public void rebox() {
-		// if there is a left child, leftw = width of the box enclosing the whole left subtree,
-		// i.e., leftw+rightw; otherwise the width is the node radius plus some additional
+		// if there is a left child, leftw = width of the box enclosing the
+		// whole left subtree,
+		// i.e., leftw+rightw; otherwise the width is the node radius plus some
+		// additional
 		// space called xspan
 		leftw = (left == null) ? D.xspan + D.radius : left.leftw + left.rightw;
 		// rightw is computed analogically
@@ -145,8 +153,8 @@ public class BSTNode extends Node {
 	}
 
 	/**
-	 * Calculate the coordinates of each node from the widths of boxes
-	 * around them and direct the nodes to their new positions. 
+	 * Calculate the coordinates of each node from the widths of boxes around
+	 * them and direct the nodes to their new positions.
 	 */
 	private void repos() {
 		if (isRoot()) {
@@ -160,8 +168,7 @@ public class BSTNode extends Node {
 			D.y2 = this.toy;
 		}
 		if (left != null) {
-			left.goTo(this.tox - left.rightw, this.toy + 2 * D.radius
-							+ D.yspan);
+			left.goTo(this.tox - left.rightw, this.toy + 2 * D.radius + D.yspan);
 			left.repos();
 		}
 		if (right != null) {
@@ -175,20 +182,122 @@ public class BSTNode extends Node {
 		reboxTree();
 		repos();
 	}
-	
+
 	/**
-	 * Find the node at coordinates (x,y).
-	 * This is used to identify the node that has been clicked by user.
+	 * Find the node at coordinates (x,y). This is used to identify the node
+	 * that has been clicked by user.
 	 */
 	public BSTNode find(int x, int y) {
-		if (inside(x,y)) return this;
+		if (inside(x, y))
+			return this;
 		if (left != null) {
 			BSTNode tmp = left.find(x, y);
-			if (tmp != null) return tmp;
+			if (tmp != null)
+				return tmp;
 		}
 		if (right != null) {
 			return right.find(x, y);
 		}
 		return null;
+	}
+
+	/**
+	 * First traverse of tree in fbtr
+	 * 
+	 * Sets a proper level to self and sons
+	 * 
+	 * @param level
+	 *            current level in tree
+	 */
+	public void fTRFirst(int level) {
+		this.level = level;
+		this.xofffromparent = 0;
+		this.thread = false;
+		if (left != null)
+			left.fTRFirst(level + 1);
+		if (right != null)
+			right.fTRFirst(level + 1);
+	}
+
+	/**
+	 * Second traverse of tree in fbtr
+	 * 
+	 * Sets threads with help from extreme nodes. Node is extreme when is lay at
+	 * the highest level and is leftmost/rightmost.
+	 * 
+	 * Simplified (divide & conquer principle): - work out left and right
+	 * subtrees - get extreme nodes from left and right subtree - set a new
+	 * thread if required
+	 * 
+	 * @return Leftmost and rightmost node on the deepest level of a tree rooted
+	 *         by this node
+	 */
+	public ExtremePair fTRSecond() {
+		ExtremePair result = new ExtremePair();
+		ExtremePair fromLeftSubtree = null, fromRightSubtree = null;
+
+		if (left != null)
+			fromLeftSubtree = left.fTRSecond();
+		if (right != null)
+			fromRightSubtree = right.fTRSecond();
+		if (isLeaf()) {
+			result.a.left = this;
+			result.a.right = this;
+		} else {
+			// is not a leaf! so at least one subtree must not be empty
+			if (left == null) {
+				result.a.left = fromRightSubtree.a.left;
+				result.a.right = fromRightSubtree.a.right;
+				return result;
+			}
+
+			if (right == null) {
+				result.a.left = fromLeftSubtree.a.left;
+				result.a.right = fromLeftSubtree.a.right;
+				return result;
+			}
+
+			/*
+			 * general switch: we want to make a thread iff one of extremes is
+			 * deeper than others. We assume that threads from subtrees are set
+			 * properly.
+			 */
+			// left subtree is more shallow than right subtree
+			if (true) {
+
+			} else
+			// right subtree is more shallow then left subtree
+			if (true) {
+
+			} else
+			// subtrees have the same height
+			{
+				result.a.left = fromLeftSubtree.a.left;
+				result.a.right = fromRightSubtree.a.right;
+			}
+
+		}
+		return result;
+	}
+
+	/**
+	 * Third traverse in fbtr
+	 * 
+	 * Calculates offset from parent
+	 */
+	public void fTRThird() {
+
+	}
+
+	/**
+	 * Fourth traverse in fbtr
+	 * 
+	 * Calculates real position from relative
+	 * 
+	 * @param xcoordinate
+	 *            real x coordinate of this node
+	 */
+	public void fTRFourth(int xcoordinate) {
+
 	}
 }
