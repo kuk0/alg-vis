2713e48bb2ed8ec73c7be55cb6752501a8707c61
Jakub Kovac <kuko@ksp.sk>
2012-01-21 11:27:43 -0500

    icombobox

---
+++ src/algvis/internationalization/IComboBox.java
@@ -0,0 +1,25 @@
+package algvis.internationalization;
+
+import javax.swing.JComboBox;
+
+public class IComboBox extends JComboBox implements LanguageListener {
+	private static final long serialVersionUID = 8795452558528688577L;
+	Languages L;
+	String[] choices;
+
+	public IComboBox(Languages L, String[] choices) {
+		super(choices);
+		this.L = L;
+		this.choices = choices;
+		languageChanged();
+		L.addListener(this);
+	}
+
+	@Override
+	public void languageChanged() {
+		removeAllItems();
+		for (int i = 0; i < choices.length; ++i) {
+			this.addItem(L.getString(choices[i]));
+		}
+	}
+}
