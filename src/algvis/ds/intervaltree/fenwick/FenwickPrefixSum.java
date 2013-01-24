dd94b1b2f2943fd3a43621c648b802d96ac1b7d4
Laco Pápay <laco.papay@gmail.com>
2013-01-24 15:44:05 +0100

    Improve messages for prefix sum and show idx calculation. Partial fix for #1 and #2.

--- src/algvis/ds/intervaltree/fenwick/FenwickPrefixSum.java
+++ src/algvis/ds/intervaltree/fenwick/FenwickPrefixSum.java
@@ -15,10 +15,9 @@ public class FenwickPrefixSum extends Algorithm {
 
 	@Override
 	public void runAlgorithm() throws InterruptedException {
-		// TODO custom header?
 		setHeader("findsum", 1, idxmax);
 		
-		// TODO rewrite and translate messages
+		addNote("fenwicksumnote");
 		
 		int sum = 0;
 		int idx = idxmax;
@@ -31,19 +30,48 @@ public class FenwickPrefixSum extends Algorithm {
 			
 			// Add to sum
 			sum += node.getStoredValue();
+			// TODO show the current sum and it's interval
 			addStep("fenwicksumstep", idx, node.getStoredValue());
 			node.mark();
 			pause();
 			node.unmark();
 			
+			// Show the new idx calculation
+			addNote("fenwicksumnextidx", getPrettyIdx(idx, idxmax));
+			
 			// Move to next interval (disjunct from current one)
 			int newidx = idx - (idx & -idx);
-			addStep("fenwicksumnextidx", newidx, idx, -idx);
 			idx = newidx;			
 		}
 		
 		
 		addNote("fenwicksumdone", sum);
+	}
+	
+	private static String[] getPrettyIdx(int idx, int idxmax)
+	{
+		// Get the original length to make it uniform
+		int length = Integer.toBinaryString(idxmax).length();
+		String zeros = new String(new char[length]).replace('\0', '0');
+		
+		// Old idx binary string
+		String sidx = (zeros + Integer.toBinaryString(idx));
+		sidx = sidx.substring(sidx.length() - length);
+		
+		// New idx binary string
+		int newidx = idx - (idx & -idx);
+		String snewidx = (zeros + Integer.toBinaryString(newidx));
+		snewidx = snewidx.substring(snewidx.length() - length);
+		
+		// Find the index of the last "1"
+		int i = 0;
+		for(int d = idx & -idx; d > 0; i++, d /= 2);		
+		
+		// Highlight the changed digit
+		sidx = sidx.substring(0, length - i) + "<u>1</u>" + sidx.substring(length - i + 1);
+		snewidx = snewidx.substring(0, length - i) + "<u>0</u>" + snewidx.substring(length - i + 1);
+		
+		return new String [] {sidx, idx+"", snewidx, newidx+""};		
 	}
 
 }
