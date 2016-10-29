6cf93b261d174a4b0bdb18a706a1cb80831b2cd4
Nguyen Duc Huy <duchuy@huy-laptop.(none)>
2016-10-29 16:53:05 +0700

    Add integration test class for BST data structure

---
+++ src/test/java/algvis/ds/dictionaries/bst/BSTTest.java
@@ -0,0 +1,256 @@
+package algvis.ds.dictionaries.bst;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import algvis.ui.BaseIntegrationTest;
+
+public class BSTTest extends BaseIntegrationTest {
+
+    BST bst;
+    int rootKeyValue, leftKeyValue, rightKeyValue;
+    
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        selectDsMenuByName(BST.dsName);
+        bst = (BST) getActiveDataStructure();
+        clearActivePanel();
+
+        keys = new int[] {100, 25, 125, 200, 45, 115, 15};
+        rootKeyValue = keys[0];
+        leftKeyValue = keys[1];
+        rightKeyValue = keys[2];
+    }
+    
+    @Test
+    public void testInsertRoot() throws Exception {
+        insert(rootKeyValue);
+
+        assertNotNull(bst);
+
+        BSTNode rootNode = bst.getRoot();
+        assertNotNull(rootNode);
+        assertEquals(rootKeyValue, rootNode.getKey());
+
+        assertTrue(rootNode.isLeaf());
+        assertFalse(rootNode.isLeft());
+    }
+    
+    @Test
+    public void testInsertRootAndLeft() throws Exception {
+        insert(rootKeyValue);
+        insert(leftKeyValue);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode leftNode = rootNode.getLeft();
+
+        assertFalse(rootNode.isLeaf());
+
+        assertNotNull(leftNode);
+        assertEquals(leftKeyValue, leftNode.getKey());
+        assertTrue(leftNode.isLeaf());
+        assertTrue(leftNode.isLeft());
+
+        assertNull(rootNode.getRight());
+    }
+    
+    @Test
+    public void testInsertRootAndRight() throws Exception {
+        insert(rootKeyValue);
+        insert(rightKeyValue);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+
+        assertFalse(rootNode.isLeaf());
+
+        assertNotNull(rightNode);
+        assertEquals(rightKeyValue, rightNode.getKey());
+        assertTrue(rightNode.isLeaf());
+        assertFalse(rightNode.isLeft());
+
+        assertNull(rootNode.getLeft());
+    }
+    
+    @Test
+    public void testInsertRootLeftRight() throws Exception {
+        insert(rootKeyValue);
+        insert(leftKeyValue);
+        insert(rightKeyValue);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode leftNode = rootNode.getLeft();
+        BSTNode rightNode = rootNode.getRight();
+
+        assertFalse(rootNode.isLeaf());
+
+        assertNotNull(leftNode);
+        assertEquals(leftKeyValue, leftNode.getKey());
+        assertTrue(leftNode.isLeaf());
+        assertTrue(leftNode.isLeft());
+
+        assertNotNull(rightNode);
+        assertEquals(rightKeyValue, rightNode.getKey());
+        assertTrue(rightNode.isLeaf());
+        assertFalse(rightNode.isLeft());
+    }
+    
+    @Test
+    public void testStructureAndOrder() throws Exception {
+        insert(rootKeyValue);
+        insert(leftKeyValue);
+        insert(rightKeyValue);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode leftNode = rootNode.getLeft();
+        BSTNode rightNode = rootNode.getRight();
+
+        assertTrue(rootNode.testStructure());
+        assertTrue(rootNode.testOrder());
+
+        leftNode.setKey(leftKeyValue + rightKeyValue);
+        assertFalse(rootNode.testOrder());
+
+        leftNode.setKey(leftKeyValue);
+        rightNode.setKey(leftKeyValue - 1);
+        assertFalse(rootNode.testOrder());
+
+        leftNode.setParent(null);
+        assertFalse(rootNode.testStructure());
+
+        leftNode.setParent(rootNode);
+        rightNode.setParent(null);
+        assertFalse(rootNode.testStructure());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        for (int key : keys) {
+            insert(key);
+        }
+        delete(rightKeyValue);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+        assertNotEquals(rightKeyValue, rightNode.getKey());
+        assertEquals(200, rightNode.getKey());
+        assertFalse(rightNode.isLeaf());
+
+        delete(rightNode.getLeft().getKey());
+        assertTrue(rightNode.isLeaf());
+
+        delete(leftKeyValue);
+        BSTNode leftNode = rootNode.getLeft();
+        assertNotEquals(leftKeyValue, leftNode.getKey());
+        assertEquals(45, leftNode.getKey());
+        assertFalse(leftNode.isLeaf());
+
+        delete(leftNode.getKey());
+        leftNode = rootNode.getLeft();
+        assertTrue(leftNode.isLeaf());
+
+        delete(rootKeyValue);
+        rootNode = bst.getRoot();
+        assertNotEquals(rootKeyValue, rootNode.getKey());
+        assertEquals(200, rootNode.getKey());
+
+        rootKeyValue = rootNode.getKey();
+        delete(rootKeyValue);
+        rootNode = bst.getRoot();
+        assertNotEquals(rootKeyValue, rootNode.getKey());
+        assertEquals(15, rootNode.getKey());
+
+        rootKeyValue = rootNode.getKey();
+        delete(rootKeyValue);
+        rootNode = bst.getRoot();
+        assertNull(rootNode);
+
+        // delete anything when being null
+        delete(keys[0]);
+    }
+    
+    @Test
+    public void testDeleteRightNodeWithOnlyLeftChild() throws Exception {
+        keys = new int[] {10, 5, 20, 15};
+        insertArray(0, keys.length);
+        delete(keys[2]);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+        assertTrue(rightNode.isLeaf());
+        assertEquals(keys[3], rightNode.getKey());
+    }
+    
+    @Test
+    public void testDeleteRightNodeWithOnlyRightChild() throws Exception {
+        keys = new int[] {10, 5, 15, 20};
+        insertArray(0, keys.length);
+        delete(keys[2]);
+
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+        assertTrue(rightNode.isLeaf());
+        assertEquals(keys[3], rightNode.getKey());
+    }
+    
+    @Test
+    public void testDeleteRightNodeWithBothChildrenCase01() throws Exception {
+        // rightNode has both left and right children
+        // right child of rightNode, rrNode, has a left child
+        // rrNode's left child also has a right child
+        keys = new int[] {10, 20, 15, 35, 25, 30};
+        insertArray(0, keys.length - 1);
+        delete(keys[1]);
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+        BSTNode rrNode = rightNode.getRight();
+        BSTNode rrLeftNode = rrNode.getLeft();
+        assertEquals(keys[4], rightNode.getKey());
+        assertEquals(keys[3], rrNode.getKey());
+        assertEquals(keys[5], rrLeftNode.getKey());
+    }
+    
+    @Test
+    public void testDeleteRightNodeWithBothChildrenCase02() throws Exception {
+        // rightNode has both left and right children
+        // right child of rightNode, rrNode, has only a right child
+        // rrNode's right child also has a right child
+        keys = new int[] {10, 20, 15, 25, 30};
+        insertArray(0, keys.length - 1);
+        delete(keys[1]);
+        BSTNode rootNode = bst.getRoot();
+        BSTNode rightNode = rootNode.getRight();
+        BSTNode rrNode = rightNode.getRight();
+        assertEquals(keys[3], rightNode.getKey());
+        assertEquals(keys[4], rrNode.getKey());
+        assertTrue(rrNode.isLeaf());
+    }
+    
+    @Test
+    public void deleteNonExistentKey() throws Exception {
+        keys = new int[] {10, 5, 8};
+        insertArray(0, keys.length - 1);
+
+        int nonExistentKey = 20;
+        delete(nonExistentKey);
+        BSTNode rootNode = bst.getRoot();
+        assertEquals(keys[0], rootNode.getKey());
+        assertNotNull(rootNode.getLeft());
+        assertNull(rootNode.getRight());
+
+        nonExistentKey = 3;
+        delete(nonExistentKey);
+        rootNode = bst.getRoot();
+        BSTNode leftNode = rootNode.getLeft();
+        assertNull(leftNode.getLeft());
+        assertNotNull(leftNode.getRight());
+    }
+}
