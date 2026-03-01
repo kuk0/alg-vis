6233870a39c43068a77e6c310bf97c1a872ae7ea
Kubo Kovac <jakub.kovac@wincent.co>
2026-03-01 16:03:51 +0100

    refactor IntervalFindMin

--- src/algvis/ds/intervaltree/IntervalFindMin.java
+++ src/algvis/ds/intervaltree/IntervalFindMin.java
@@ -27,9 +27,9 @@ import algvis.ui.view.REL;
 public class IntervalFindMin extends IntervalAlg {
     private int i;
     private int j;
-    private IntervalNode maxi;
-    private static final int ninf = -2147483648;
-    private static final int pinf = 2147483647;
+    // Aggregation result node. Its meaning depends on
+    // the configured aggregation type: maximum, minimum or sum.
+    private IntervalNode aggregate;
 
     public IntervalFindMin(IntervalTree T, int i, int j) {
         super(T);
@@ -51,19 +51,19 @@ public class IntervalFindMin extends IntervalAlg {
         if (j > T.numLeafs || j < 1) {
             j = T.numLeafs;
         }
+        // Initialize the aggregation accumulator node according to the
+        // configured aggregation type (max/min/sum).
         if (T.minTree == mimasuType.MAX) {
-            maxi = new IntervalNode(T, ninf, ZDepth.NODE);
+            aggregate = new IntervalNode(T, Integer.MIN_VALUE, ZDepth.NODE);
             setHeader("findmax", i, j);
         } else if (T.minTree == mimasuType.MIN) {
-            maxi = new IntervalNode(T, pinf, ZDepth.NODE);
+            aggregate = new IntervalNode(T, Integer.MAX_VALUE, ZDepth.NODE);
             setHeader("findmin", i, j);
         } else {
-            maxi = new IntervalNode(T, 0, ZDepth.NODE);
+            aggregate = new IntervalNode(T, 0, ZDepth.NODE);
             setHeader("findsum", i, j);
         }
         T.markColor(T.root, i, j);
-        // System.out.println(i + " " + j);
-        // System.out.println(T.root.b + " " + T.root.e);
 
         // kazdy vrchol ma zapamatany interval, ktory reprezentuje (je to
         // b-e+1=2^k
@@ -75,15 +75,15 @@ public class IntervalFindMin extends IntervalAlg {
             find(T.root, i, j);
             pause();
             if (T.minTree == mimasuType.MAX) {
-                addStep(T, 200, REL.TOP, "maximum", maxi.getKeyS());
+                addStep(T, 200, REL.TOP, "maximum", aggregate.getKeyS());
             } else if (T.minTree == mimasuType.MIN) {
-                addStep(T, 200, REL.TOP, "minimum", maxi.getKeyS());
+                addStep(T, 200, REL.TOP, "minimum", aggregate.getKeyS());
             } else if (T.minTree == mimasuType.SUM) {
-                addStep(T, 200, REL.TOP, "sumimum", maxi.getKeyS());
+                addStep(T, 200, REL.TOP, "sum", aggregate.getKeyS());
             }
             if (T.minTree != mimasuType.SUM) {
                 T.unfocus(T.root);
-                maxi.mark();
+                aggregate.mark();
                 T.markColor(T.root, i, j);
             }
             pause();
@@ -98,12 +98,13 @@ public class IntervalFindMin extends IntervalAlg {
 
         w.mark();
 
+        // Case 1: disjoint (node interval is completely outside query)
         if ((w.b > e) || (w.e < b)) {
             if (w.getKey() != Node.NOKEY) {
                 addStep(w, REL.TOP, "intervalout", "" + i, "" + j, w.getKeyS(),
-                    "" + w.b, "" + w.e); // mimo intervalu
+                    "" + w.b, "" + w.e);
             } else {
-                addStep(w, REL.TOP, "intervalempty", "" + w.b, "" + w.e); // prazdny vrchol
+                addStep(w, REL.TOP, "intervalempty", "" + w.b, "" + w.e);
             }
             w.focused = focusType.TOUT;
             pause();
@@ -112,30 +113,33 @@ public class IntervalFindMin extends IntervalAlg {
             return;
         }
 
-        if ((w.b >= b) && (w.e <= e)) {
+        // Case 2: contained (node interval is fully inside query)
+        else if ((w.b >= b) && (w.e <= e)) {
             if (T.minTree != mimasuType.SUM) {
-                if (w.prec(maxi)) {
-                    maxi = w;
+                if (w.prec(aggregate)) {
+                    aggregate = w;
                 }
             } else {
-                maxi.setKey(maxi.getKey() + w.getKey());
+                aggregate.setKey(aggregate.getKey() + w.getKey());
             }
             addStep(w, REL.TOP, "intervalin", "" + i, "" + j, w.getKeyS(),
-                "" + w.b, "" + w.e); // dnu intervalu
+                "" + w.b, "" + w.e);
             w.focused = focusType.TIN;
             pause();
             return;
         }
 
-        if ((w.b <= b) || (w.e >= e)) {
+        // Case 3: partial intersection (node interval overlaps query but is not contained)
+        else {
             addStep(w, REL.TOP, "intervalpart", "" + i, "" + j, w.getKeyS(),
-                "" + w.b, "" + w.e); // neprazdny prienik
+                "" + w.b, "" + w.e);
             w.focused = focusType.TOUT;
             pause();
             w.focused = focusType.TWAIT;
             find(w.getLeft(), b, e);
             find(w.getRight(), b, e);
         }
+
         w.unmark();
         w.focused = focusType.FALSE;
     }
